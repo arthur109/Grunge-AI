a7b0e298421a8c9cf406611338eaa9a211fa77fa
arthur109 <arthurfaj@gmail.com>
2017-07-13 15:22:15 -0700

    First Commit

---
+++ grunge_trainer.py
@@ -0,0 +1,190 @@
+# Import Libraries
+from os.path import isfile
+import os
+import numpy as np
+import keras
+from keras.models import load_model
+from keras.utils import plot_model
+import helper_functions
+import progressbar
+
+
+# Initialize Parameters for custumization
+# The Dimension Of The Image
+params = {'Image Dimensions': {'x': 128, 'y': 128}}
+params = {
+    # Ignore this
+    'Image Dimensions': params['Image Dimensions'],
+    # Print Debug Output
+    'Debug Mode': True,
+    # how many images in each batch
+    'Batch Size': 3,
+    # size of small batches in each normal batch:
+    'Micro Batch Size': 2,
+    # How many times to repatedly train on the images in the Repition folder
+    'Repetition Image Repitions': 10,
+    # Directory of the images that it will be repeatedly trained on.
+    'Compressed Repition Training Folder Directory': 'dataset/Repeated_Images',
+    # directory containing jpg
+    'Compressed Train Image Directory': 'dataset/train',
+    # directory containing jpg
+    'Compressed Evaluation Image Directory': 'dataset/eval',
+    # directory containing h5 files used in training
+    'Decompressed Train Image Directory': 'processed/train',
+    # directory containing h5 files used in evaluation
+    'Decompressed Evaluation Image Directory': 'processed/eval',
+    # directory containing h5 files used for repition
+    'Decompressed Repition Training Folder Directory': 'processed/Repeated_Images',
+    # The Number of neurons in each layer
+    'Layer Dimensions': [params['Image Dimensions']['x'] * params['Image Dimensions']['y'], 7000, 3000, 900, 3000, 7000, params['Image Dimensions']['x'] * params['Image Dimensions']['y']],
+    # the neural networks optimizer
+    'Network Optimizer': 'adam',
+    # the neural networks loss function
+    'Network Loss': 'mse',
+    # what to measure when training the networks
+    'Network Metrics': ['accuracy'],
+    # The Activation Function Use After Each Layer
+    'Network Activation Function': 'sigmoid',
+    # Should The Program Load The Previoulsy Saved Model
+    'Should Load Model': False,
+    # Where to load the model from
+    'Path To Model To Load': 'savedModel.h5',
+    # Where to save the model
+    'Path To Export Model To': 'savedModel.h5',
+    # Where To Save Evaluations
+    'Evaluation Save Directory': 'output',
+    # name of the saved neural network diagram
+    'Model Image Filename': 'model.png'
+}
+
+# uses the fucntion make_batches in the helper_functions folder to convert the images to h5 files in the batch sizes
+# (also does it for the evaluation images)
+helper_functions.make_batches(params, [
+    # training
+    [params['Compressed Train Image Directory'],
+        params['Decompressed Train Image Directory']],
+    # evaluation
+    [params['Compressed Evaluation Image Directory'],
+        params['Decompressed Evaluation Image Directory']],
+    # repition
+    [params['Compressed Repition Training Folder Directory'],
+        params['Decompressed Repition Training Folder Directory']]
+])
+
+# Get list of directorys of training batches
+train_batches = helper_functions.loadH5sFromFolder(
+    params['Decompressed Train Image Directory'])
+# Get list of directorys of evaluation batches
+eval_batches = helper_functions.loadH5sFromFolder(
+    params['Decompressed Evaluation Image Directory'])
+# Get list of directorys of repition batches
+repit_batches = helper_functions.loadH5sFromFolder(
+    params['Decompressed Repition Training Folder Directory'])
+
+
+if params['Debug Mode']:
+    print 'train shape: ' + str(np.asarray(train_batches).shape)
+    print 'evaluation shape: ' + str(np.asarray(eval_batches).shape)
+
+'''
+# tells user if they did not supply enough images for the training
+if len(train_batches) < params['Batch Number']:
+    print('BATCH ERROR:')
+    print('I was told to run through ' + str(params['Batch Number']) + ' batches, yet I can only create ' + str(len(train_batches)) +
+          ' batches with the number of images supplied. I need ' + str((params['Batch Number'] - len(train_batches)) * params['Batch Size']) + ' more images')
+
+# tells user if program has enough evaluation images
+if len(eval_batches) < params['Batch Number']:
+    print('EVALUATION ERROR:')
+    print('I was told to run through ' + str(params['Batch Number']) + ' batches, yet I can only create ' + str(len(eval_batches)) +
+          ' evaluation sets with the number of images supplied. I need ' + str((params['Batch Number'] - len(eval_batches)) * params['Number Of Images To Evaluate On']) + ' more images')
+'''
+# Creates the Neural Network
+model = helper_functions.makeModel(params)
+
+# Loads previous model if it exists and the option is chosen
+if params['Should Load Model'] and isfile(params['Path To Model To Load']):
+    model.load_weights(params['Path To Model To Load'])
+
+# prints error if it should load model but the path to the model is false:
+if params['Should Load Model'] and (isfile(params['Path To Model To Load']) is False):
+    print 'Was told to load teh model but the path givin is false, so the model was not loaded.'
+
+# Prints a summary of the model and plot a picture if debug mode is on
+if params['Debug Mode']:
+    print model.summary()
+    plot_model(model, to_file=params['Model Image Filename'], show_shapes=True)
+
+# Main Program Loop
+#Clear Eval Output Folder
+helper_functions.clearFolder(params['Evaluation Save Directory'])
+# Make A Progress Bar
+pbar = progressbar.ProgressBar()
+# loops through the number of training batches
+for trainBatchNum in pbar(range(len(train_batches))):
+    print "Batch " + str(trainBatchNum)
+
+    # Load Current Training Batch
+    train_batch = helper_functions.loadBatch(train_batches[trainBatchNum])
+    print('Training Model')
+    # Train The Model On The Batch
+    model.fit(train_batch, train_batch, epochs=1, batch_size=params['Micro Batch Size'])
+
+    # ID Of Current Image To Be Saved
+    outImgID = 0
+    print('Evaluating')
+    for evalBatchNum in range(len(eval_batches)):
+        # Load Current Evaluation Batch
+        eval_batch = helper_functions.loadBatch(eval_batches[evalBatchNum])
+        # Generate Images On Batch
+        evaluations = model.predict(eval_batch, batch_size=1)
+        # Save Each Generated Image And Its Corresponding Input
+        for i, evaluation in enumerate(evaluations):
+            helper_functions.saveImg(eval_batch[i], params['Evaluation Save Directory'] + "/Batch " + str(
+                trainBatchNum) + "-Eval " + str(outImgID) + "-input.png", params)
+            helper_functions.saveImg(evaluation, params['Evaluation Save Directory'] + "/Batch " + str(
+                trainBatchNum) + "-Eval " + str(outImgID) + "-output.png", params)
+            outImgID += 1
+
+    print('Saving Model')
+    # saves the model
+    model.save_weights(params['Path To Export Model To'])
+
+
+print np.array(repit_batches).shape
+# For loop that does the repitions of the images in the repition folder
+outImgID = 0;
+# makes a new folder to save the repition evaluation images
+os.makedirs(params['Evaluation Save Directory']+'/repitions')
+
+for r in range(params['Repetition Image Repitions']):
+    print 'Repetition: '+str(r+1)+'/'+str(params['Repetition Image Repitions'])
+    # loop that trains through the repition images once
+    for trainBatchNum in range(len(repit_batches)):
+
+        # Load Current Training Batch
+        train_batch = helper_functions.loadBatch(repit_batches[trainBatchNum])
+
+        print('Training Model...')
+        # Train The Model On The Batch
+        model.fit(train_batch, train_batch, epochs=1, batch_size=params['Micro Batch Size'])
+
+        print('Saving Model...')
+        # saves the model
+        model.save_weights(params['Path To Export Model To'])
+
+    # ID Of Current Image To Be Saved
+        print('Evaluating...')
+        for evalBatchNum in range(len(eval_batches)):
+            # Load Current Evaluation Batch
+            eval_batch = helper_functions.loadBatch(eval_batches[evalBatchNum])
+            # Generate Images On Batch
+            evaluations = model.predict(train_batch, batch_size=1)
+            # Save Each Generated Image And Its Corresponding Input
+            for i, evaluation in enumerate(evaluations):
+
+                helper_functions.saveImg(train_batch[i], params['Evaluation Save Directory'] + "/repitions/Batch " + str(trainBatchNum) + "-Eval " + str(outImgID) + "-input-repition.png", params)
+
+                helper_functions.saveImg(evaluation, params['Evaluation Save Directory'] + "/repitions/Batch " + str(trainBatchNum) + "-Eval " + str(outImgID) + "-output-repition.png", params)
+
+                outImgID += 1
---
+++ helper_functions.py
@@ -0,0 +1,192 @@
+from os.path import isfile, join
+from os import listdir
+import scipy.misc
+from keras.layers import Dense, Activation, Flatten, Input, Reshape
+from keras.models import Model
+import h5py
+import numpy as np
+from keras.preprocessing import image
+import progressbar
+import shutil
+import os
+
+# input is a directory from which to get the files so it can return a list containg the paths of all the h5's inside it
+
+
+def loadH5sFromFolder(directory):
+    return sorted([join(directory, f)
+                   for f in listdir(directory)
+                   if (isfile(join(directory, f)) and ("part" in f))])  # returns paths of all h5 files
+
+# input is a directory from which to get the files so it can return a list contaning the paths of all the images inside it
+
+
+def loadIMGsFromFolder(directory):
+    return sorted([join(directory, f)
+                   for f in listdir(directory)
+                   if (isfile(join(directory, f)) and ("img" in f))])  # returns paths of all images in folder
+
+
+def makeModel(params):
+    # Create An Input Layer From The Image Dimensions
+    inputLayer = Input(shape=(params['Image Dimensions']['x'], params['Image Dimensions']['y'], 1))
+    # Flatten Out The Image
+    prevLayer = Reshape((params['Image Dimensions']['x'] * params['Image Dimensions']['y'], ))(inputLayer)
+
+    # Create All The Layers
+    for layerDim in params['Layer Dimensions'][:-1]:
+        prevLayer = Dense(layerDim, activation=params['Network Activation Function'])(prevLayer)
+    prevLayer = Dense(params['Layer Dimensions'][-1], activation='tanh')(prevLayer)
+    # Reshapes The Output Back Into An Image (from 1d to 2d)
+    outputLayer = Reshape((params['Image Dimensions']['x'], params['Image Dimensions']['y'], 1))(prevLayer)
+
+    # Create The Network
+    model = Model(inputs=inputLayer, outputs=outputLayer)
+    # Setup The Network
+    model.compile(optimizer=params['Network Optimizer'],
+                  loss=params['Network Loss'],
+                  metrics=params['Network Metrics'])
+
+    return model
+
+
+def saveImg(imgInput, filename, params):
+    # Make sure the array is the right shape
+    imgInput = imgInput.reshape((params['Image Dimensions']['x'], params['Image Dimensions']['y'], 1))
+    # Make the image have 3 channels
+    img = changeChannels1to3(imgInput, params)
+    # Save the image to the specified filename
+    scipy.misc.toimage(img, cmin=0.0, cmax=1).save(filename)
+
+# converts an image with only one channel(black and white) and converts it to full rgb(uduplicates the channel)
+# so it can be properly exported to an image
+
+
+def changeChannels1to3(imgInput, params):
+    # Create A Blank Image With 3 Channels
+    img = np.zeros((params['Image Dimensions']['x'], params['Image Dimensions']['y'], 3), dtype=np.float32)
+    # Loop Through Each Row In the image
+    for h, row in enumerate(imgInput):
+        # Loop Through each column in each row
+        for i, col in enumerate(row):
+            # Set All 3 Channels to the original BW value
+            for channel in col:
+                img[h][i][0] = channel
+                img[h][i][1] = channel
+                img[h][i][2] = channel
+    return img
+
+# a function that takes the path of an h5 file and return the conetents of the file in an array
+
+
+def loadBatch(path):
+    # opens the file in reading mode, hence the 'r'
+    batchH5F = h5py.File(path, 'r')
+    # gets all the data inside it
+    batch = batchH5F['data'][:]
+    # closes the file
+    batchH5F.close()
+    # returns the array of its contents
+    return batch
+
+# a function that takes in a 2d array in format:
+# [
+#    [input folder directory, output folder directory],
+#    [input folder directory, output folder directory],
+#    ...
+#    for all other things that must convert from images to h5.py files
+# ]
+# (also takes in params so it know the batch size, and evaluation batch size)
+
+
+def make_batches(params, inputOutputLocations):
+    # goes through every arra in the inputOutputLocations array
+    for pathsToImages in inputOutputLocations:
+        # gets the directorys of the input and output folders
+        inputFolder = pathsToImages[0]
+        outputFolder = pathsToImages[1]
+        # clears all previous data in the output folder using the function created below
+        clearFolder(outputFolder)
+        # uses function loadIMGsFromFolder(that was created above) to get a list of all the paths of the images in the input folder
+        imagePaths = loadIMGsFromFolder(inputFolder)
+        # creats an empty list that will contain all the images
+        imagesAsList = []
+        # adds a progress bar and titles it (so we know what its doing)
+        print 'loading images...'
+        pbar = progressbar.ProgressBar()
+        # loops therough all the image paths and displays progress bar (that is done with the pbar())
+        for i in pbar(range(len(imagePaths))):
+            # gets the image inan array using the function 'image_to_array' created below and stores it in the variable 'img'
+            img = img_to_array(imagePaths[i], params['Image Dimensions'])
+            # checks if there were no errors with the conversion process
+            if not (type(img) == 'NoneType'):
+                try:
+                    # trys to append image
+                    imagesAsList.append(img)
+                except ValueError:
+                    # if there is an error in apending it it prints 'the images path + is bad'
+                    print imagePaths[i] + " is bad"
+            else:
+                # also prints 'the images path + is bad'
+                print imagePaths[i] + " is bad"
+            # creats new np array which will contain all images (and will replace imagesAsList)
+        images = np.zeros((len(imagesAsList), params['Image Dimensions']['x'], params['Image Dimensions']['y'], 1))
+        # adds the images
+        for i in range(len(images)):
+            images[i] = imagesAsList[i]
+
+        # ex: you have 30 images, batch size = 3
+        # number of images/batch size = 30/3 = 10
+        # range(1,10) = np.array[1,2,3,4,5,6,7,8,9] (doesn't include last number)
+        # that np array * batch size = [1*3,2*3,3*3,...] = [3,6,9,12,15,18,21,24,27,30]
+        # and then splits it on that array (splits function on numbers in the array as index positions returning an array of arrays)
+        # all in that line of code, below
+        batches = np.split(images, (np.array(range(1, int(len(images) / params['Batch Size']))) * params['Batch Size']).tolist())
+
+        # goes through each array in array and writes it to a h5 file
+        for i, batch in enumerate(batches):
+            # gives it a proper name and path(output folder), 'w' means writing not reading the file
+            h5f = h5py.File(outputFolder + "/part-" + str(i).zfill(7) + ".h5", "w")
+            # adds data to file
+            h5f.create_dataset('data', data=batch)
+            # closes the file. Remeber to alwayse put this at the end
+            h5f.close()
+        images = None
+
+# a function that takes a directory and deletes all things inside it.
+
+
+def clearFolder(folder):
+    # laods an array with paths of all files
+    allFiles = [folder + "/" + f for f in listdir(folder)]
+    # goes throught each path and delets the coresponding file
+    for f in allFiles:
+        try:
+            os.remove(f)
+        except OSError:
+            shutil.rmtree(f)
+
+# a function that takes in a path and the image dimesions and outputs an array containg all the black and white pixel values in range 0 to 1
+
+
+def img_to_array(imgPth, dimensions):
+    img = None
+    try:
+        # tries to load image
+        img = image.load_img(imgPth, target_size=(dimensions['x'], dimensions['y']))
+        # converts image to array
+        img = image.img_to_array(img)
+        # scales rgb values form 0-1
+        img *= 1. / 255
+        # makes it float 16 so it takes up less space
+        img = img.astype(np.float16)
+        # delets g and b rgb values to make balck and white
+        img = np.delete(img, 1, 2)
+        img = np.delete(img, 1, 2)
+    except IOError:
+        # if it failed to load the image it prints image path is bad
+        print imgPth + " is bad"
+        # and returns nothing
+        return None
+    # if not, returns the image array
+    return img
